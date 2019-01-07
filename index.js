c617f6ac5f2c79d16c43208e80b134bbec5dad6d
Alexis Paques <alexis.paques@gmail.com>
2019-01-07 21:18:57 +0100

    show data on screen

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-
 const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 3030, clientTracking: true });
+const YeeDevice = require('yeelight-platform').Device;
+const attributes_names = ['name', 'active_mode', 'power', 'bright', 'ct', 'bg_power', 'bg_hue', 'bg_rgb', 'bg_sat', 'bg_ct'];
 
-const wss = new WebSocket.Server({ port: 3030 });
+var lights = [];
+
+function addLight(name, ip) {
+  let light = {
+    name: name,
+    device: new YeeDevice({host: ip, port: 55443,
+    attributes: attributes_names, id: lights.length+1})
+  }
+  lights.push(light);
+}
+
+function send(topic, data) {
+  let msg = {
+    "topic": topic, 
+    "data": data
+  };
+  for(c of wss.clients) {
+    c.send(JSON.stringify(msg));
+  }
+}
+
+addLight("living", "192.168.178.32");
+addLight("dining", "192.168.178.33");
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    console.log(message)
   });
-
-  ws.send('something');
+  for(light of lights) {
+    if(light.device.attributes.name != null)
+      send("heartbeat", light.device.attributes);
+  }
 });
+
+for(let light of lights) {
+  light.device.connect();
+  light.device.on('update', (attrs, light) => {
+    for(c of wss.clients) {
+      send("heartbeat", attrs);
+    }
+  });
+}
+
